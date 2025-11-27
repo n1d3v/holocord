934d01902d1c18dc03bf62d0ea87c2dbdd84d2ba
patricktbp <135556230+n1d3v@users.noreply.github.com>
2025-11-27 21:45:00 +0000

    Fixed a mistake in the activity_logon layout

---
+++ Holocord/app/src/main/java/dev/bricktapper/holocord/ClientActivity.java
@@ -0,0 +1,13 @@
+package dev.bricktapper.holocord;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClientActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+    }
+}
